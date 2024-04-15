fc2db1f8b3412e7551443299c8f280fe5681d2c9
AlagusuryaPeriyakaruppan <alagup1405@gmail.com>
2024-04-15 22:49:36 +0530

    Anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,25 @@
 
 function isAnagram(str1, str2) {
 
+  if(str1.length !== str2.length)return false;
+
+  str1=str1.toLowerCase()
+  str2=str2.toLowerCase()
+
+  //convert string to array,sort arry,convert array to string
+  let string1 = str1.split('').sort().join('');
+  let string2 = str2.split('').sort().join('');
+  console.log("STR",string1,string2);
+
+  for(let i=0;i<string1.length;i++){
+      console.log(string1[i],string2[i])
+      if(string1[i] !== string2[i]){
+        return false;
+      }
+  }
+  return true;
 }
+
+
 
 module.exports = isAnagram;
