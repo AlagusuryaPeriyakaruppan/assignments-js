53aa63ffafe4cce1f56a722132f340b14da014b9
AlagusuryaPeriyakaruppan <alagup1405@gmail.com>
2024-04-23 22:52:00 +0530

    EXPENDITURE ANALYSIS

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -3,18 +3,74 @@
   and return a list of objects where each object is unique category-wise and has total price spent as its value.
   transactions is an array where each
   Transaction - an object like 
-        {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  }
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656076800000,
+    price: 80,
+    category: 'Vehicle',
+    itemName: 'Ather',
+  },
+  {
+    id: 3,
+    timestamp: 1656076800000,
+    price: 70,
+    category: 'Clothes',
+    itemName: 'Jacket',
+  },
+  {
+    id: 4,
+    timestamp: 1656076800000,
+    price: 40,
+    category: 'Food',
+    itemName: 'Dosa',
+  },
+]
+
+// METHOD - 1:
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const Output = [];
+
+  transactions.forEach((transaction)=>{
+      //Find if the category already exist in Resultant array
+    const existingCategory = Output.find(item=>item.category === transaction.category)
+    console.log(existingCategory)
+    if(existingCategory){
+      existingCategory.totalSpent += transaction.price 
+    }else{
+      Output.push({category:transaction.category,totalSpent:transaction.price})
+    }
+  })
+
+  return Output;
 }
 
+
+
+
+
+
+
+//calculateTotalSpentByCategory(transactions)
+
 module.exports = calculateTotalSpentByCategory;
+
+
