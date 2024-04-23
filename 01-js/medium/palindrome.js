6e78cf2320839e4d1c4118d8e1be21cec1f16c91
AlagusuryaPeriyakaruppan <alagup1405@gmail.com>
2024-04-23 23:46:12 +0530

    palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,8 +3,20 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+//malayalam
 function isPalindrome(str) {
+str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+for(let i=0;i<str.length/2-1;i++){
+
+  if(str[i] !== str[str.length-i-1]){
+    return false;
+  }
+}
+
   return true;
 }
+
+//console.log(isPalindrome('Eva, can I see bees in a cave?'));
+// isPalindrome("masrdam");
 
 module.exports = isPalindrome;
