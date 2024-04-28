b9988f621815c8869aa5efa00f3aeecab5cc9567
AlagusuryaPeriyakaruppan <alagup1405@gmail.com>
2024-04-28 12:41:59 +0530

    CALCULATOR

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,65 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  // The constructor method is a special method of a class for creating and initializing an object instance of that class.
+  constructor() {
+    this.result = 0;
+  }
 
+  add(n) {
+    this.result += n;
+  }
+
+  subtract(n) {
+    this.result -= n;
+  }
+
+  multiply(n) {
+    this.result *= n;
+  }
+
+  divide(n) {
+    if(n===0){
+      throw new Error("Division by zero is not allowed")
+    }
+    this.result /= n;
+  }
+
+  clear() {
+    this.result = 0
+  }
+
+  calculate(expression){
+    //trim the starting and trailing spaces and then remove all the occurences of spaces
+    // \s+ : This is a regular expression pattern that matches one or more whitespace characters, including spaces, tabs, and newlines.
+    expression = expression.replace(/\s+/g, '').trim();
+
+
+  if(!/^[0-9+*/\-().]+$/.test(expression)){
+      throw new Error("Invalid Expression")
+  }
+
+    if (expression.includes('/0')) {
+      throw new Error('Division by zero is not allowed');
+    }
+
+   this.result = eval(expression)
+  }
+
+
+  getResult() {
+    return this.result;
+  }
+}
+
+// const calculator = new Calculator();
+// calculator.calculate('10 +   2 *    (   6 - (4 + 1) / 2) + 7');
+
+// console.log(calculator.getResult());
+
+// const calculator2 = new Calculator();
+// calculator2.calculate('10 / 0');
+
+// console.log(calculator2.getResult());
 module.exports = Calculator;
