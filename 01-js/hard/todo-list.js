232434e7a0ecc20e73901830d41d1a3d4d863cee
AlagusuryaPeriyakaruppan <alagup1405@gmail.com>
2024-04-28 13:50:59 +0530

    TODO LIST

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,53 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos=[];
+  }
 
+  add(todo){
+    this.todos.push(todo)
+  }
+
+  remove(indexOfTodo){
+
+    const todoBeRemoved = this.get(indexOfTodo)
+    if(!todoBeRemoved)return;
+    
+    const filteredTodos = this.todos.filter((todo)=>todo!==todoBeRemoved);
+    this.todos = filteredTodos;
+  }
+
+  update(index,updatedTodo){
+    if(index > this.todos.length-1)return;
+    return this.todos[index] = updatedTodo
+  }
+
+  getAll(){
+    return this.todos
+  }
+
+  get(indexOfTodo){
+    return this.todos[indexOfTodo] ? this.todos[indexOfTodo]:null
+  }
+
+  clear(){
+    return this.todos=[];
+  }
 }
 
+
+const todo = new Todo();
+todo.add("Apple")
+todo.add("Orange")
+todo.add("Banana")
+todo.add("Grapes")
+todo.update(1,"Kiwi")
+todo.update(5,"Unknown Fruit")
+console.log(todo.getAll())
+
+
+
+
 module.exports = Todo;
+
